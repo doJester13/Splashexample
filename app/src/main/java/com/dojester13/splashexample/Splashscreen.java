fce5e97421df2c0081768e91f3ebf48151ae6954
Erik Minarini <Erik@MBP-di-Erik.homenet.telecomitalia.it>
2016-04-30 15:49:57 +0200

    right way

--- app/src/main/java/com/dojester13/splashexample/Splashscreen.java
+++ app/src/main/java/com/dojester13/splashexample/Splashscreen.java
@@ -7,25 +7,15 @@ import android.os.Bundle;
 
 public class Splashscreen extends AppCompatActivity {
 
-    private static int TIME = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
 
-        //this handler execute run after TIME milliseconds
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                //just an Intent form here to Main activity
-                Intent i = new Intent(Splashscreen.this, MainActivity.class);
-                startActivity(i);
-                finish();
-            }
-
-        }, TIME);
+        Intent i = new Intent(Splashscreen.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 
 }
